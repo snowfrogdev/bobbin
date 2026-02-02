0e3c28c5df941c0ba2d7f3ecf97eb02e6d795547
Philippe Vaillancourt <phil@snowfrog.dev>
2026-02-02 18:08:57 +0000

    fix(docker): Pin Rust nightly for WASM build compatibility
    Rust 1.93+ removed -Zbuild-std support for wasm32-unknown-emscripten.
    Pin to nightly-2026-01-20 which still supports this feature, required
    for building cdylib crate type for Godot WASM exports.

--- docker/build.sh
+++ docker/build.sh
@@ -55,7 +55,7 @@ fi
 
 case "$TARGET" in
     windows)
-        cargo +nightly build --manifest-path bindings/godot/Cargo.toml \
+        cargo build --manifest-path bindings/godot/Cargo.toml \
             --target-dir target --target x86_64-pc-windows-gnu $FEATURES $CARGO_PROFILE_FLAG
         cp target/x86_64-pc-windows-gnu/$PROFILE/bobbin_godot.dll "$BIN_DIR/bobbin_godot$SUFFIX.dll"
         ;;
@@ -66,9 +66,9 @@ case "$TARGET" in
         # Set rustflags for WASM target (side module for Godot)
         export CARGO_TARGET_WASM32_UNKNOWN_EMSCRIPTEN_RUSTFLAGS="-C link-args=-pthread -C target-feature=+atomics -C link-args=-sSIDE_MODULE=2 -C panic=abort -Zlink-native-libraries=no -Cllvm-args=-enable-emscripten-cxx-exceptions=0"
         # Use 'wasm' profile (size-optimized, no LTO)
-        # Note: -Zbuild-std is no longer supported with wasm32-unknown-emscripten target
-        cargo +nightly build --manifest-path bindings/godot/Cargo.toml \
-            --target-dir target --profile wasm --target wasm32-unknown-emscripten
+        # Note: Requires pinned nightly (see Dockerfile) - newer nightlies removed -Zbuild-std support for emscripten
+        cargo build --manifest-path bindings/godot/Cargo.toml \
+            -Zbuild-std=std,panic_abort --target-dir target --profile wasm --target wasm32-unknown-emscripten
         # Run wasm-opt for aggressive size optimization (more efficient than LTO for WASM)
         WASM_FILE="target/wasm32-unknown-emscripten/wasm/bobbin_godot.wasm"
         WASM_OPT="$EMSDK/upstream/bin/wasm-opt"
@@ -80,7 +80,7 @@ case "$TARGET" in
         cp "$WASM_FILE" "$BIN_DIR/bobbin_godot.wasm"
         ;;
     linux)
-        cargo +nightly build --manifest-path bindings/godot/Cargo.toml --target-dir target $FEATURES $CARGO_PROFILE_FLAG
+        cargo build --manifest-path bindings/godot/Cargo.toml --target-dir target $FEATURES $CARGO_PROFILE_FLAG
         cp target/$PROFILE/libbobbin_godot.so "$BIN_DIR/libbobbin_godot$SUFFIX.so"
         ;;
     all)
